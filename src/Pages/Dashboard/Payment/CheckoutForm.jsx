bafd4dd4234cf7c88cd38ef04404199b85013f32
mbdullah <mdabdullah162005@gmail.com>
2025-06-25 17:47:50 +0600

    included final payment successfull craiteria, please sent it

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,19 +1,40 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
+import { useParams } from "react-router";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const CheckoutForm = () => {
   const stripe = useStripe();
-  const element = useElements();
+  const elements = useElements();
   const [error, setError] = useState("");
+  const {id} = useParams();
+  const axiosSecure = useAxiosSecure();
+
+  const {isPending, data: parcelInfo = {}} = useQuery({
+    queryKey: ['parcels', id],
+    queryFn: async () => {
+      const {data} = await axiosSecure.get(`/parcels/${id}`)
+      return data;
+    }
+  })
+
+  if(isPending){
+    return <span>Loading...</span>
+  }
+
+  const amount = parcelInfo.cost;
+  const amountInCents = amount*100;
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!stripe || !element) {
+    if (!stripe || !elements) {
       return;
     }
 
-    const card = element.getElement(CardElement);
+    const card = elements.getElement(CardElement);
 
     if (!card) {
       return;
@@ -32,6 +53,34 @@ const CheckoutForm = () => {
       console.log("paymentMethod", paymentMethod);
     }
 
+    // Create payment intent
+    const {data} = await axiosSecure.post('/create-payment-intent', {
+      amountInCents,
+      id
+    })
+
+    const clientSecret = data?.clientSecret;
+
+    const result = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Jenny Rosen',
+        },
+      }
+    });
+
+     if (result.error) {
+      console.log(result.error.message);
+    } else {
+      if (result.paymentIntent.status === 'succeeded') {
+        console.log('Payment succeeded!');
+        console.log(result);
+      }
+    }
+
+    
+
   };
   return (
     <form
@@ -44,7 +93,7 @@ const CheckoutForm = () => {
         type="submit"
         disabled={!stripe}
       >
-        pay for parcel
+        Pay ${amount}
       </button>
       {
         error && <p className="text-red-500">{error}</p>
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -2,7 +2,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import CheckoutForm from './CheckoutForm';
-const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+const stripePromise = loadStripe(import.meta.env.VITE_payment_key);
 
 const Payment = () => {
     return (
